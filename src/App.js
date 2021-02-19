8c73430620aacd5d9d145aed661d46f4658a199c
ToshifumiIde <toshifumiide198765@gmail.com>
2021-02-19 23:45:28 +0900

    create react hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const App = () => {
-  return <div className="">This is atemplate for React App</div>;
+  const [value, setValue] = useState("");
+  const [count, setCount] = useState(0);
+  return (
+    <div className="">
+      This is item plate for React App
+      <form action="">
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+      </form>
+      <div className="">
+        Count:{count}
+        <button
+        // onClick={setCount(count + 1)}
+        >
+          +
+        </button>
+        <button
+        // onClick={}
+        >-</button>
+      </div>
+    </div>
+  );
 };
