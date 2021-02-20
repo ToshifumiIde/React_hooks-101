4ac2de47127574904dd0cc8e2fe7315b2044fb0d
ToshifumiIde <toshifumiide198765@gmail.com>
2021-02-20 23:30:39 +0900

    set counter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,41 @@ import React, { useState } from "react";
 export const App = () => {
   const [value, setValue] = useState("");
   const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const increment2 = () => {
+    setCount((previousCount) => previousCount + 1);
+  };
+  const decrement2 = () => {
+    setCount((previousCount) => previousCount - 1);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValue("");
+  };
+  const setReset = () => {
+    setCount(0);
+  };
+  const setDouble = () => {
+    setCount(count * 2);
+  };
+  const setMultiple3 = () => {
+    if (count % 3 === 0) {
+      setCount(count / 3);
+    } else {
+      return;
+    }
+  };
+
   return (
-    <div className="">
+    <>
       This is item plate for React App
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           value={value}
@@ -14,18 +45,27 @@ export const App = () => {
             setValue(e.target.value);
           }}
         />
+        <button>送信</button>
       </form>
+      <ul>
+        <li>todo</li>
+        <li>todo</li>
+        <li>todo</li>
+      </ul>
+      Count:{count}
       <div className="">
-        Count:{count}
-        <button
-        // onClick={setCount(count + 1)}
-        >
-          +
-        </button>
-        <button
-        // onClick={}
-        >-</button>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
       </div>
-    </div>
+      <div className="">
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div className="">
+        <button onClick={setReset}>Reset</button>
+        <button onClick={setDouble}>Double</button>
+        <button onClick={setMultiple3}>/3</button>
+      </div>
+    </>
   );
 };
