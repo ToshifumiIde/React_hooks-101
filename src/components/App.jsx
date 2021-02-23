02f11b5ec01bcc7b7ffc6ffb57d2c506e077cfb6
ToshifumiIde <toshifumiide198765@gmail.com>
2021-02-23 19:15:09 +0900

    setup react context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,24 @@
 import React, { useReducer } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { EventForm } from "./EventForm";
-import { reducer } from "../reducers";
+import AppContext from "../contexts/AppContext";
 import { Events } from "./Events";
+import { reducer } from "../reducers";
+
+console.log(AppContext);
 
 export const App = () => {
   const [state, dispatch] = useReducer(reducer, []);
   //今回の実装では第3引数は不要
   return (
     <>
+    <AppContext.Provider value={"hello I'm a provider. "}>
       <div className="container-fluid">
         <EventForm state={state} dispatch={dispatch} />
         <Events state={state} dispatch={dispatch} />
       </div>
+    </AppContext.Provider>
     </>
   );
 };
+
